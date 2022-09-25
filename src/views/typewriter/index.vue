70c366a11d1492d49a67b8f3da0a18b1c5540204
maozhiling <1613360707@qq.com>
2022-09-25 21:40:43 +0800

    init

---
+++ src/views/typewriter/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="content">
+        asdasd
+    </div>
+</template>
+  
+<script>
+//像素字体
+import "nes.css/css/nes.min.css";
+export default {
+    components: {
+
+    }
+}
+</script>
+<style scoped lang="scss">
+.content {
+    position: absolute;
+    right: 0;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    background-color: black;
+}
+</style>
+  
